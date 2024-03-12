c200beea938b9e54162f5ec48c7ec8160b2b7a48
Muna0027 <adan0027@algonquinlive.com>
2024-03-12 18:29:03 -0400

    new commit

---
+++ aks.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "cst8918h09group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "munaCluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "munaDns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 3
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+
+  sensitive = true
+}
